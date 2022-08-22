40570b4b25551ac64bedaf8873e162a8afa1ee94
Bakaullah-Ansari <bakaullah@gmail.com>
2022-08-22 18:38:42 +0530

    This is my first commit

---
+++ first-project/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div><h1>Welcome to the VUE js project</h1></div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
